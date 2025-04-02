0033678d5dea9a2b6f82c6b70ee338714339a065
Luiz Henrique <luizhenrique@zgsolucoes.com.br>
2025-04-01 23:02:26 -0300

    feat: Add basic Logger interface and DefaultLogger implementation

---
+++ internal/usecase/log.go
@@ -0,0 +1,26 @@
+package usecase
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	// DefaultWaitTime Default wait time between operations
+	DefaultWaitTime = 2 * time.Second
+)
+
+type Logger interface {
+	Info(msg string, args ...any)
+	Error(msg string, err error, args ...any)
+}
+
+type DefaultLogger struct{}
+
+func (l *DefaultLogger) Info(msg string, args ...any) {
+	fmt.Printf(msg+"\n", args...)
+}
+
+func (l *DefaultLogger) Error(msg string, err error, args ...any) {
+	fmt.Printf(msg+": %v\n", append(args, err)...)
+}
