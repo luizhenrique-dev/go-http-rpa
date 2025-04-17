7aac9d27f46c3bee3b4e4e39db204e9b78523180
Luiz Henrique <luizhenrique@zgsolucoes.com.br>
2025-04-17 00:00:10 -0300

    feat: Add TaskProcessQuiz to handle quiz processing pipeline (start and answer)

---
+++ rpa_quiz/task_process_quiz.go
@@ -0,0 +1,23 @@
+package rpaquiz
+
+import (
+	"github.com/luizhenriquees/go-http-rpa/engine"
+	httprequest "github.com/luizhenriquees/go-http-rpa/http_request"
+)
+
+type TaskProcessQuiz struct {
+	*engine.PipelinedTasks
+}
+
+func NewTaskProcessQuiz(headers httprequest.Headers, params engine.Parameters) *TaskProcessQuiz {
+	pipeline := &TaskProcessQuiz{
+		PipelinedTasks: engine.NewPipelinedTasks(
+			"process_quiz",
+			make([]engine.Task, 0, 2),
+		),
+	}
+
+	pipeline.AddTask(NewTaskStartQuiz(headers, params))
+	pipeline.AddTask(NewTaskAnswerQuiz(headers, params))
+	return pipeline
+}
